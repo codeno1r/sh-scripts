6e8bfb0f6d583004e86411bed19738ad52fa52c0
codeno1r <gvarrworks@gmail.com>
2025-03-25 07:22:30 +0800

    backup

--- nightlight.sh
+++ nightlight.sh
@@ -5,21 +5,20 @@ MONITOR="eDP-1"
 
 # Check if state file exists; if not, create it with default "off"
 if [ ! -f "$STATE_FILE" ]; then
-    echo "off" > "$STATE_FILE"
+  echo "off" >"$STATE_FILE"
 fi
 
 # Read the current state
 STATE=$(cat "$STATE_FILE")
 
 if [ "$STATE" = "off" ]; then
-    # Turn night light ON
-    xrandr --output "$MONITOR" --gamma 1.0:0.7:0.5
-    echo "on" > "$STATE_FILE"
-    echo ""
+  # Turn night light ON
+  xrandr --output "$MONITOR" --gamma 1.0:0.6:0.6
+  echo "on" >"$STATE_FILE"
+  echo ""
 else
-    # Turn night light OFF
-    xrandr --output "$MONITOR" --gamma 1:1:1
-    echo "off" > "$STATE_FILE"
-    echo ""
+  # Turn night light OFF
+  xrandr --output "$MONITOR" --gamma 1:1:1
+  echo "off" >"$STATE_FILE"
+  echo ""
 fi
-
--- win10.sh
+++ win10.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 virsh --connect=qemu:///system start win10 && virt-manager --connect qemu:///system --show-domain-console win10
+# virt-manager --connect qemu:///system --show-domain-console win10
